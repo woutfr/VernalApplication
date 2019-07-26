815cda6a9a553aa72decb75e7200365fa9c52304
wout <wout_frijters@hotmail.com>
2019-07-26 20:18:50 +0200

    11

--- app/src/main/java/com/example/vernaljobs/Navview.kt
+++ app/src/main/java/com/example/vernaljobs/Navview.kt
@@ -1,17 +1,16 @@
 package com.example.vernaljobs
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.core.view.GravityCompat
-import androidx.appcompat.app.ActionBarDrawerToggle
+import android.view.Menu
 import android.view.MenuItem
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import android.view.Menu
-
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 class Navview : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     val manager = supportFragmentManager
 
---
+++ app/src/main/java/com/example/vernaljobs/TermsAndConditions.kt
@@ -0,0 +1,12 @@
+package com.example.vernaljobs
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TermsAndConditions : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms_and_conditions)
+    }
+}
